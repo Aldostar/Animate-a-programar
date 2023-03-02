bedacedada196aef46d2d69cf3326c5ebefc5744
Aldostar <124640213+Aldostar@users.noreply.github.com>
2023-03-02 10:52:49 -0300

    Add files via upload

---
+++ ANIMATE A PROGRAMAR/tarea clase 10/javascript.js
@@ -0,0 +1,11 @@
+var nombre = "Aldana";
+var apellido = "Romero";
+console.log (nombre + " " + apellido);
+
+
+var numero1 = 365
+var numero2 = 72
+var suma = numero1 + numero2
+console.log (suma)
+var numero3 = 13
+console.log (suma + numero3)
