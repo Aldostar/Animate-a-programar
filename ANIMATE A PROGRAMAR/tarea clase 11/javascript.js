de9f72feb8123a4b3e13c18dfd3a1536b56e647a
Aldostar <124640213+Aldostar@users.noreply.github.com>
2023-03-02 13:35:52 -0300

    Add files via upload

---
+++ ANIMATE A PROGRAMAR/tarea clase 11/javascript.js
@@ -0,0 +1,28 @@
+let topping = ["Oreo", "KitKat", "Kinder",];
+let PrecioFinal;
+let helado = 300;
+let precio;
+let usuarioEleccion = "KitKat"
+let mensaje; 
+
+
+if (topping [0] == usuarioEleccion){
+mensaje= "el valor de tu topping es $" + [precio = 10];
+}
+else if  (topping [1] == usuarioEleccion) {
+    mensaje= "el valor de tu topping es $" + [precio = 15]
+}
+else if (topping [2]== usuarioEleccion) { 
+    mensaje= "el valor de tu topping es $" + [precio = 25]
+}
+
+else { mensaje ="No tenemos ese topping"}
+
+console.log (mensaje)
+
+precioFinal= precio + helado
+
+
+console.log ( "El helado cuesta $" + precioFinal)
+
+
